5e6468cb7678423906b8453484152f6e208f1d9d
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2026-06-29 15:09:02 +0200

    Update _command.sh
    vereinfacht..

--- _command.sh
+++ _command.sh
@@ -1,71 +1,83 @@
-cd /media/pi/1.42.6-25556/yt-dlp/
+#!/bin/bash
+cd /media/psw/1.42.6-25556/yt-dlp/
 echo "beginning______________________________________________________________________________"
 echo $(date -u)
-#LGR
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive LGR.txt https://www.youtube.com/c/Lazygamereviews/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --reject-title 'LGR Plays' --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
 
-#LGR BLERBS
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive LGRBlerbs.txt https://www.youtube.com/c/lgrblerbs/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150  --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
+# ============================================================
+# KANALLISTE - hier neue Kanäle eintragen
+# Format: "ARCHIVNAME,URL" oder "ARCHIVNAME,URL,--reject-title 'TITEL'"
+# ============================================================
+CHANNELS=(
+    "DFretro,https://www.youtube.com/playlist?list=PLY9cZ8nX4xmlpd1RE8_toocbU9cND7U-A"
+    "scienceelf,https://www.youtube.com/@TheScienceElf/videos"
+    "AVGN,https://www.youtube.com/playlist?list=PL2B009153AC977F90"
+    "uthermal,https://www.youtube.com/@uthermal/videos,--reject-title 'Stormgate|Battle Aces|ZeroSpace'"
+    "uthermal2,https://www.youtube.com/@uthermal2/videos,--reject-title 'Stormgate|Battle Aces|ZeroSpace'"
+    "Ahoy,https://www.youtube.com/@XboxAhoy/videos,--reject-title 'Guide'"
+    "LGR,https://www.youtube.com/c/Lazygamereviews/videos,--reject-title 'LGR Plays'"
+    "LGRBlerbs,https://www.youtube.com/c/lgrblerbs/videos"
+    "GamingHistorian,https://www.youtube.com/c/gaminghistorian/videos"
+    "techmoan,https://www.youtube.com/c/Techmoan/videos"
+    "RMC,https://www.youtube.com/c/RMCRetro/videos"
+    "kurzgesagt,https://www.youtube.com/c/kurzgesagtDE/videos"
+    "NostalgiaNerd,https://www.youtube.com/c/nostalgianerd/videos"
+    "The8BitGuy,https://www.youtube.com/c/The8BitGuy/videos"
+    "LastWeekTonight,https://www.youtube.com/user/LastWeekTonight/videos"
+    "ThisDoesNotCompute,https://www.youtube.com/c/ThisDoesNotCompute/videos"
+    "ThisWeekInRetro,https://www.youtube.com/c/ThisWeekInRetro/videos"
+    "videogamedunkey,https://www.youtube.com/user/VideoGameDunkey/videos"
+    "WrestlingWithGaming,https://www.youtube.com/c/WrestlingWithGaming/videos"
+    "NewMind,https://www.youtube.com/c/NewMind/videos"
+    "SNESdrunk,https://www.youtube.com/c/SNESdrunk/videos"
+    "strafefox,https://www.youtube.com/c/strafefox/videos"
+    "posy,https://www.youtube.com/c/PosyMusic/videos"
+    "vwestlife,https://www.youtube.com/c/vwestlife/videos"
+    "wendover,https://www.youtube.com/c/Wendoverproductions/videos"
+    "techtangents,https://www.youtube.com/c/AkBKukU/videos"
+    "vswitchzero,https://www.youtube.com/@vswitchzero/videos"
+    "miketech,https://www.youtube.com/@miketech1024/videos"
+    "necroware,https://www.youtube.com/@necro_ware/videos"
+    "epictronics,https://www.youtube.com/@Epictronics1/videos"
+    "bitsundbolts,https://www.youtube.com/@bitsundbolts/videos"
+    "dosstorm,https://www.youtube.com/@DOSStorm/videos"
+    "januscycle,https://www.youtube.com/@JanusCycle/videos"
+    "65scribe,https://www.youtube.com/@65scribe/videos"
+    "pixelpipes,https://www.youtube.com/@PixelPipes/videos"
+    "retrospector,https://www.youtube.com/@RetroSpector78/videos"
+)
 
-#GAMINGHISTORIAN
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive GamingHistorian.txt https://www.youtube.com/c/gaminghistorian/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
+DENO=/home/psw/.deno/bin/deno
+YT_DLP=/usr/local/bin/yt-dlp
+OUTPUT='%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s'
 
-#TECHMOAN
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive techmoan.txt https://www.youtube.com/c/Techmoan/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150  --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
+for entry in "${CHANNELS[@]}"; do
+    ARCHIVE=$(echo "$entry" | cut -d',' -f1)
+    URL=$(echo "$entry"     | cut -d',' -f2)
+    EXTRA=$(echo "$entry"   | cut -d',' -f3-)
 
-#RMC
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive RMC.txt https://www.youtube.com/c/RMCRetro/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
+    # Kein drittes Feld: EXTRA ist dann gleich ARCHIVE
+    [ "$EXTRA" = "$ARCHIVE" ] && EXTRA=""
 
-#Kurzgesagt
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive kurzgesagt.txt https://www.youtube.com/c/kurzgesagtDE/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
+    echo "$ARCHIVE"
 
-#Nostalgia Nerd
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive NostalgiaNerd.txt https://www.youtube.com/c/nostalgianerd/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#8BitGuy
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive The8BitGuy.txt https://www.youtube.com/c/The8BitGuy/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#LastWeekTonight
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive LastWeekTonight.txt https://www.youtube.com/user/LastWeekTonight/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#This does not compute
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive ThisDoesNotCompute.txt https://www.youtube.com/c/ThisDoesNotCompute/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#This week in Retro
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive ThisWeekInRetro.txt https://www.youtube.com/c/ThisWeekInRetro/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#VideoGameDunkey
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive videogamedunkey.txt https://www.youtube.com/user/VideoGameDunkey/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#Wrestling With Gaming
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive WrestlingWithGaming.txt https://www.youtube.com/c/WrestlingWithGaming/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#New Mind
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive NewMind.txt https://www.youtube.com/c/NewMind/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#SNESdrunk
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive SNESdrunk.txt https://www.youtube.com/c/SNESdrunk/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#Strafefox
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive strafefox.txt https://www.youtube.com/c/strafefox/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#Posy
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive posy.txt https://www.youtube.com/c/PosyMusic/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#vwestlife
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive vwestlife.txt https://www.youtube.com/c/vwestlife/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#tom scott
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive TomScott.txt https://www.youtube.com/c/TomScottGo/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#Wendover Productions
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive wendover.txt https://www.youtube.com/c/Wendoverproductions/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#LowSpec Gamer
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --convert-thumbnails jpg --sponsorblock-mark all --embed-metadata --download-archive lowSpecGamer.txt https://www.youtube.com/c/LowSpecGamer/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
-
-#Tech Tangents
-sudo yt-dlp -S "+res:480,ext:mp4,vcodec:av1" --embed-thumbnail --embed-metadata --download-archive techtangents.txt https://www.youtube.com/c/AkBKukU/videos -o '%(channel)s/%(upload_date>%Y-%m-%d ) s%(title)s.%(ext)s' --windows-filenames --trim-filenames 150 --embed-subs --sub-langs en --remux-video mkv --playlist-reverse -q
+    $YT_DLP \
+        -S "+res:480,ext:mp4,vcodec:av1" \
+        --embed-thumbnail \
+        --convert-thumbnails jpg \
+        --embed-metadata \
+        --download-archive "${ARCHIVE}.txt" \
+        "$URL" \
+        -o "$OUTPUT" \
+        --windows-filenames \
+        --trim-filenames 150 \
+        --embed-subs \
+        --sub-langs en \
+        --remux-video mkv \
+        --playlist-reverse \
+        -q \
+        --js-runtimes "deno:${DENO}" \
+        $EXTRA
+done
 
 echo $(pwd)
 echo $(date -u)
